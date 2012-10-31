3b50ea03ed66b744fd3b3dbc58b18d5b525abe16
Ramesh Chandra <rameshch@pdos.csail.mit.edu>
2012-10-30 22:42:45 -0400

    git pull

--- build_pkgs.sh
+++ build_pkgs.sh
@@ -11,8 +11,8 @@ for repo in filterer filterer-web-client; do
 done
 
 ## build filterer packages
-(cd $ROOT/filterer; mvn clean package -Dmaven.test.skip=true) || exit 1
+(cd $ROOT/filterer; git pull; mvn clean package -Dmaven.test.skip=true) || exit 1
 
 ## main branch of filterer-web-client is webclient
-(cd $ROOT/filterer-web-client; git checkout webclient) || exit 1
+(cd $ROOT/filterer-web-client; git checkout webclient; git pull) || exit 1
 
